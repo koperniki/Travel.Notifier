d068ef6933fd0923299ef9df2e055b49cd4b243a
koperniki <ypsilonik@mail.ru>
2021-02-27 13:44:59 +0700

    add initial message

--- Travel.Notifier/Hubs/NotifierHub.cs
+++ Travel.Notifier/Hubs/NotifierHub.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Travel.Notifier.Services;
 
 namespace Travel.Notifier.Hubs
 {
     public class NotifierHub : Hub
     {
+        private readonly ButtonStateService _stateService;
+
+        public NotifierHub(ButtonStateService stateService)
+        {
+            _stateService = stateService;
+        }
+
         public override async Task OnConnectedAsync()
         {
-             Console.WriteLine(Context.ConnectionId);
-             await base.OnConnectedAsync();
+            await base.OnConnectedAsync();
+            Console.WriteLine(Context.ConnectionId);
+            await Task.Delay(1000);
+            await Clients.Client(Context.ConnectionId).SendAsync("transferstatedata", _stateService.GetButtons());
+
         }
     }
 }
